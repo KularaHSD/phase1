be3562df53b0433e1743a7927ec2b044047288fd
KularaHSD <136809392+KularaHSD@users.noreply.github.com>
2023-06-22 14:23:51 -0400

    Update GameOver.js

--- bunny/src/scenes/GameOver.js
+++ bunny/src/scenes/GameOver.js
@@ -16,6 +16,9 @@ export default class GameOver extends Phaser.Scene
         this.add.text(width * 0.5, height * 0.5, 'Game Over', { fontSize: 48})
         .setOrigin(0.5)
 
+        this.add.text(width * 0.5, height * 0.58, 'Press Space-Bar to Restart', {fontSize: 24})
+        .setOrigin(0.5)
+
         this.input.keyboard.once('keydown-SPACE', () => {this.scene.start('game')})
     }
 }
