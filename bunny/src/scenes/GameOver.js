35665f17a23be37bfdf854504e8d24dd4d8739e4
KularaHSD <136809392+KularaHSD@users.noreply.github.com>
2023-06-22 14:28:12 -0400

    Add files via upload

--- bunny/src/scenes/GameOver.js
+++ bunny/src/scenes/GameOver.js
@@ -15,7 +15,6 @@ export default class GameOver extends Phaser.Scene
 
         this.add.text(width * 0.5, height * 0.5, 'Game Over', { fontSize: 48})
         .setOrigin(0.5)
-
         this.add.text(width * 0.5, height * 0.58, 'Press Space-Bar to Restart', {fontSize: 24})
         .setOrigin(0.5)
 
